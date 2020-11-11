ebf1686f6eeb912e567de68f175dcbd8c93df5df
LUIZ ASSIS <luiz.assis.ti@gmail.com>
2020-11-11 12:42:12 -0300

    Adicionar configuração com o protocolo Ssl
    O motivo para adicionar essa configuração foi por tentar realizar a conexão com o CloudAMQP
    
    Links:
    + https://varejointeligente.atlassian.net/browse/HB-94

---
+++ src/Hubee.MessageBroker.Sdk/Core/Configurations/ConfigureRabbitMQSsl.cs
@@ -0,0 +1,32 @@
+﻿using Hubee.MessageBroker.Sdk.Core.Models;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using System.Security.Authentication;
+
+namespace Hubee.MessageBroker.Sdk.Core.Configurations
+{
+    internal class ConfigureRabbitMQSsl : IConfigureMessageBroker
+    {
+        public void Configure(IServiceCollection services, HubeeMessageBrokerConfig config)
+        {
+            services.AddMassTransit(x => x.AddBus(context => Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.UseHealthCheck(context);
+                cfg.Host(config.HostName, config.Port, config.VirtualHost,
+                     h =>
+                     {
+                         h.Username(config.UserName);
+                         h.Password(config.Password);
+
+                         h.UseSsl(s =>
+                         {
+                             s.Protocol = SslProtocols.Tls12;
+                         });
+                     });
+
+                cfg.ExchangeType = ExchangeType.Fanout;
+            })));
+        }
+    }
+}

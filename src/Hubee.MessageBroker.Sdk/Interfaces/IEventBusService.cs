45dadcbdd0ad1ad5c2a0836578cc6d09d5d6fb75
Marcos Carvalho <marcos@triv.io>
2026-01-28 12:41:31 -0300

    feat: add publish overload to customize trace message data [skip ci]

--- src/Hubee.MessageBroker.Sdk/Interfaces/IEventBusService.cs
+++ src/Hubee.MessageBroker.Sdk/Interfaces/IEventBusService.cs
@@ -7,7 +7,14 @@ namespace Hubee.MessageBroker.Sdk.Interfaces
     public interface IEventBusService
     {
         Task Publish<T>(object message) where T : class;
-        Task Publish(object message, Type type);
         Task Publish<T>(object message, CancellationToken cancellationToken) where T : class;
+
+        Task Publish<T>(object message, Guid? messageId, Guid? correlationId) where T : class;
+        Task Publish<T>(object message, Guid? messageId, Guid? correlationId, CancellationToken cancellationToken) where T : class;
+
+        Task Publish(object message, Type type);
+
+        Task Publish(object message, Type type, Guid? messageId, Guid? correlationId);
+        Task Publish(object message, Type type, Guid? messageId, Guid? correlationId, CancellationToken cancellationToken);
     }
 }
